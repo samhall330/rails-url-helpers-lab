1bdca6eb71ab3f1925a087b2a93ccf66bb4d511a
Sam Hall <mann.samantha11@gmail.com>
2021-01-14 23:25:19 -0500

    commit tree f2bd21379c5b86119ab2b3330e201c545ec654f0

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+student1 = Student.create(first_name: "Paula", last_name: "Abdul")
+student2 = Student.create(first_name: "Spike", last_name: "Lee")
+student3 = Student.create(first_name: "Aaron", last_name: "Rodgers")
